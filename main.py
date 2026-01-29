e13030201675ed4ab2c7e3ad7d34770c5a9bba2d
abrokecube <58220690+abrokecube@users.noreply.github.com>
2026-01-28 23:42:18 -0600

    add dotenv configuration

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import uvicorn
 from api import app
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def main():
     """
@@ -8,7 +12,7 @@ def main():
     """
     # The monitor thread is already started when importing 'app' from 'api'
     # because of the module-level code in api.py.
-    uvicorn.run(app, host="0.0.0.0", port=8110)
+    uvicorn.run(app, host=os.getenv("SERVER_HOST", "0.0.0.0"), port=int(os.getenv("SERVER_PORT", 8110)))
 
 if __name__ == "__main__":
     main()
